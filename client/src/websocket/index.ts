30ebecd8f6bf2bb16bfc4741220a4fbf5eb0c98d
dalersaidov <daler.saidov.developer@gmail.com>
2025-08-05 13:24:24 +0500

    refactor: remove unused code

--- client/src/websocket/index.ts
+++ client/src/websocket/index.ts
@@ -1,15 +1,11 @@
-// import { useNewMediaFrameSub } from "./eventSubs/newMediaFrameSub.ts";
 import { type BaseOutgoingMessage, type BaseIncomingMessage } from "./types";
 import { createOutgoingMessageCreator } from "./createOutgoingMessageCreator.ts";
 import websocketClient from "./WebsocketClient.ts";
-import { createMessageSender } from "./createMessageSender.ts";
 import { incomingMessageIsOfTypeError } from "./utils.ts";
 
 export {
-  // useNewMediaFrameSub,
   createOutgoingMessageCreator,
   websocketClient,
-  createMessageSender,
   type BaseOutgoingMessage,
   type BaseIncomingMessage,
   incomingMessageIsOfTypeError,
